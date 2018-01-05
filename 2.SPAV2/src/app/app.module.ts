16eebb54b39eac5ed26b406252eb1e5b5288f902
saidabalsells <temporal>
2018-01-05 22:03:50 +0100

    [class51] - create heroes service

--- 2.SPAV2/src/app/app.module.ts
+++ 2.SPAV2/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-//servicios
+// servicios
+import { HeroesService  } from './services/heroes.service';
 
-//rutas
+// rutas
 import { APP_ROUTING} from './app.routes';
 
 // componentes
@@ -26,7 +27,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 2.SPAV2/src/app/components/heroes/heroes.component.ts
+++ 2.SPAV2/src/app/components/heroes/heroes.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styles: []
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(private _heroesService: HeroesService) { }
 
 }
