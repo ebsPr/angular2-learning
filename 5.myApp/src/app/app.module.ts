52cc0e7a5d1c06946a7287bcc52ef5f2f7bffeda
saidabalsells <temporal>
2018-01-12 20:21:28 +0100

    [class 106]  init ionic app and first components

---
+++ 5.myApp/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+
+import { TabsPage } from '../pages/tabs/tabs';
+import { PendientesComponent } from '../pages/pendientes/pendientes.component';
+import { TerminadosComponent } from '../pages/terminados/terminados.component';
+
+import { ListaDeseosService } from '../app/services/lista-deseos-service';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    PendientesComponent,
+    TerminadosComponent,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    PendientesComponent,
+    TerminadosComponent,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    ListaDeseosService,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
